e20ab3a7f8908ecc7820510de116778088a298be
chriamue <chriamue@users.noreply.github.com>
2022-01-23 08:00:28 +0000

    deploy: f597546c38565b2ad0e6a2902ba9dc9ae9cccf05

--- main.js
+++ main.js
@@ -821,7 +821,7 @@ const defaultConfig = async () => {
 /******/ 					promises.push(installedWasmModuleData);
 /******/ 				else {
 /******/ 					var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 					var req = fetch(__webpack_require__.p + "" + {"17":{"716":"198125ed82336438305a"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"17":{"716":"70cb2f8fade6a0d513bf"}}[chunkId][wasmModuleId] + ".module.wasm");
 /******/ 					var promise;
 /******/ 					if(importObject && typeof importObject.then === 'function' && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
